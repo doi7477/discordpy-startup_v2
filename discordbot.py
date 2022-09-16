97bcb18bb0c4603090eb216631bae09f2c28b1cc
doi7477 <70732724+doi7477@users.noreply.github.com>
2022-09-16 19:17:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -176,6 +176,12 @@ async def loops():
             await channel.send('@everyone \r\n本日の「ギルド活動 施設物アップデート」はお済みですか？')
     return
 
+async def fn():
+    loop.start()
+    
+loop_ = asyncio.get_event_loop()
+loop_.run_until_complete(fn())
+
 ####################
 #起動時の処理
 ####################
@@ -471,12 +477,6 @@ async def on_message(message):
                 return
 
 #実行
-loops.start()
-
-#async def fn():
-#    loop.start()
-    
-#loop_ = asyncio.get_event_loop()
-#loop_.run_until_complete(fn())
+#loops.start()
 client.run(token)
 
